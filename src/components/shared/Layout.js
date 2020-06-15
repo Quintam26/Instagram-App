7655865563a3fa0637f950e9bcbf4aa282ed0ffe
Mario Quintana <pyrros2000@gmail.com>
2020-06-15 15:06:12 -0700

    Added Navbar componenet to not found page with Insta logo

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SEO from '../shared/Seo';
+import Navbar from '../shared/Navbar';
 import { useLayoutStyles } from '../../styles';
 
 function Layout({ children, title }) {
@@ -8,6 +9,7 @@ function Layout({ children, title }) {
   return (
     <section className={classes.section}>
       <SEO title={title}/>
+      <Navbar />
       <main className={classes.main}>
         <section className={classes.childrenWrapper}>
           <div className={classes.children}>{children}</div>
