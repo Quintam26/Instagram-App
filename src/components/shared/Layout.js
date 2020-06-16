557912c669b88c8b8451484a3157be95ca16b64b
Mario Quintana <pyrros2000@gmail.com>
2020-06-16 10:57:48 -0700

    Completed Not Found page by adding text and link to root route

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -3,14 +3,14 @@ import SEO from '../shared/Seo';
 import Navbar from '../shared/Navbar';
 import { useLayoutStyles } from '../../styles';
 
-function Layout({ children, title }) {
+function Layout({ children, title, marginTop = 60 }) {
   const classes = useLayoutStyles();
 
   return (
     <section className={classes.section}>
       <SEO title={title}/>
       <Navbar />
-      <main className={classes.main}>
+      <main className={classes.main} style={{ marginTop }}>
         <section className={classes.childrenWrapper}>
           <div className={classes.children}>{children}</div>
         </section>
