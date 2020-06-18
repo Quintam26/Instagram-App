56f831693ee990e52f1584ae5e6f468257a40fc0
Mario Quintana <pyrros2000@gmail.com>
2020-06-18 16:28:08 -0700

    building out single post layout: image, buttons, likes

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -1,10 +1,51 @@
-import React from "react";
-import { useFeedPostStyles } from "../../styles";
+import React from 'react';
+import { useFeedPostStyles } from '../../styles';
+import UserCard from '../shared/UserCard';
+import { MoreIcon, CommentIcon, ShareIcon } from '../../icons';
+import { Link } from 'react-router-dom;'
+import { Typography } from '@material-ui/core';
 
-function FeedPost() {
-  useFeedPostStyles();
+function FeedPost({ post }) {
+  const classes = useFeedPostStyles();
+  const { media, id, likes } = post;
 
-  return <div>FeedPost</div>;
+  return (
+    <>
+      <article className={classes.article}>
+        {/* Feed Post Header*/}
+        <div className={classes.postHeader}>
+          <UserCard />
+          <MoreIcon className={classes.moreIcon} />
+        </div>
+        {/* Feed Post Image*/}
+        <div>
+          <img src={media} alt='Post media' className={classes.image} />
+        </div>
+        {/* Feed Post Buttons*/}
+        <div className={classes.postButtonWrapper}>
+        <div className={classes.postButtons}>
+          <LikeButton />
+          <Link to={`/p/${id}`}>
+            <CommentIcon />
+          </Link>
+          <ShareIcon />
+          <SaveButton />
+        </div>
+        <Typography className={classes.like} variant="subtitle2">
+          <span></span>
+        </Typography>
+        </div>
+      </article>
+    </>
+  );
+}
+
+function LikeButton() {
+
+}
+
+function SaveButton() {
+
 }
 
 export default FeedPost;
