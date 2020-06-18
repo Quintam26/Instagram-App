7da8837ba586a045597301f19348c56e8be36211
Mario Quintana <pyrros2000@gmail.com>
2020-06-18 10:43:48 -0700

    started feed page and rendering dummy data

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { useFeedPageStyles } from "../styles";
+import Layout from '../components/shared/Layout';
+import FeedPost from '../components/feed/FeedPost';
+import { defaultPost } from '../data';
 
 function FeedPage() {
-  useFeedPageStyles();
+  const classes = useFeedPageStyles();
 
-  return <div>FeedPage</div>;
+  return <Layout>
+    <div className={classes.container}>
+      <div>
+        {Array.from({ length: 5 }, () => defaultPost).map(post => (
+          <FeedPost key={post.id} post={post}/>
+        ))}
+      </div>
+    </div>
+  </Layout>;
 }
 
 export default FeedPage;
