de11d73ccd3e4b6ba33c48e99a75ae4d5b20abc1
Mario Quintana <pyrros2000@gmail.com>
2020-06-24 16:46:37 -0700

    Can now close notifications by not clicking the notifications button

--- src/components/notification/NotificationList.js
+++ src/components/notification/NotificationList.js
@@ -4,12 +4,15 @@ import { defaultNotifications } from '../../data';
 import { Grid, Avatar, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import FollowButton from '../shared/FollowButton';
+import useOutsideClick from '@rooks/use-outside-click';
 
-function NotificationList() {
+function NotificationList({ handleHideList }) {
   const classes = useNotificationListStyles();
+  const listContainerRef = React.useRef();
+  useOutsideClick(listContainerRef, handleHideList);
 
   return (
-    <Grid className={classes.listContainer} container>
+    <Grid ref={listContainerRef} className={classes.listContainer} container>
       {defaultNotifications.map((notification) => {
         const isLike = notification.type === 'like';
         const isFollow = notification.type === 'follow';
